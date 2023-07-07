46b3bb47da8fcefb1b7608c0eafaf2631020a2d3
HaroonMalik771 <itsharoonmalik143@gmail.com>
2023-07-07 16:29:15 +0500

    Create Simple_Calculator.py

---
+++ Simple_Calculator.py
@@ -0,0 +1,24 @@
+# Here is the calculator in Python
+
+number1 = float(input("Enter the first number:"))
+number2 = float(input("Enter the second number:"))
+operation = input("Enter the weather you want to solve\nAddition\nSubtraction\nMultiplication\nDivision")
+if operation == 'Addition':
+    sum = number1 + number2
+    print("The sum of"+str(number1)+"and"+str(number2)+"is",sum)
+elif operation == 'Subtraction':
+    sub = number1 - number2
+    print("The subtaction of"+str(number1)+"and"+str(number2)+"is",sub)
+elif operation == 'Multiplication':
+    Mul = number1 * number2
+    print("The Multiplication of"+str(number1)+"and"+str(number2)+"is",Mul)   
+elif operation == 'Division':
+    if number2!=0:
+        Div = number1 / number2
+        print("The Division of"+str(number1)+"and"+str(number2)+"is",Div)
+    else:
+        print("The number2 is not zero")
+else:
+    print("Invalid Input!")        
+
+            
