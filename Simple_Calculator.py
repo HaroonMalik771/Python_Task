a67348c9f3ee3ba66bed189a35ad6eb24559f403
HaroonMalik771 <itsharoonmalik143@gmail.com>
2023-07-07 16:42:20 +0500

    update

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -2,22 +2,22 @@
 
 number1 = float(input("Enter the first number:"))
 number2 = float(input("Enter the second number:"))
-operation = input("Enter the weather you want to solve\nAddition\nSubtraction\nMultiplication\nDivision")
+operation = input("Enter the weather you want to solve\nAddition\nSubtraction\nMultiplication\nDivision\n")
 if operation == 'Addition':
     sum = number1 + number2
-    print("The sum of"+str(number1)+"and"+str(number2)+"is",sum)
+    print("The sum of "+str(number1)+" and "+str(number2)+" is ",sum)
 elif operation == 'Subtraction':
     sub = number1 - number2
-    print("The subtaction of"+str(number1)+"and"+str(number2)+"is",sub)
+    print("The subtraction of "+str(number1)+" and "+str(number2)+" is ",sub)
 elif operation == 'Multiplication':
     Mul = number1 * number2
-    print("The Multiplication of"+str(number1)+"and"+str(number2)+"is",Mul)   
+    print("The Multiplication of "+str(number1)+" and "+str(number2)+" is ",Mul)
 elif operation == 'Division':
     if number2!=0:
         Div = number1 / number2
-        print("The Division of"+str(number1)+"and"+str(number2)+"is",Div)
+        print("The Division of "+str(number1)+" and "+str(number2)+" is ",Div)
     else:
-        print("The number2 is not zero")
+        print("The Second Number Should not zero")
 else:
     print("Invalid Input!")        
 
